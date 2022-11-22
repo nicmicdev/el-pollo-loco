e1af5027e36c79b3d11b8b108ba406f6f80d4b2d
Niclas Michel <niclas.michel@MacBook-Pro.local>
2022-11-22 20:09:18 +0800

    placed first objects on canvas

--- models/world.class.js
+++ models/world.class.js
@@ -7,10 +7,38 @@ class World {
     new Chicken(),
     new Chicken(),
     ]
+    canvas;
+    ctx;
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.draw();
+
+
+    }
 
 
     
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y,this.character.height,this.character.width);
+
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y,enemy.height,enemy.width);
+
+
+            
+        });
+
+        let self = this;
+        
+        // draw() wird immer wieder aufgerufen
+        requestAnimationFrame(function(){
+            self.draw();
+        })
 
     }
 }
